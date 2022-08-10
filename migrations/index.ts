2abe899cf1682cf56231514faa4c77ab655fb77e
eduwr <eduwric@gmail.com>
2022-08-10 00:37:39 -0300

    improve migration system

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,14 +1,37 @@
 import migration001 from "./001_create-users-table";
+import migration002 from "./002_auto-update-timestamps";
+import pg from "pg";
 
-const [_, __, action] = process.argv;
+export const client = new pg.Client({
+  connectionString: "postgres://postgres:example@localhost:5432/db",
+});
 
-switch (action) {
-  case "up":
-    migration001.up();
-    break;
-  case "down":
-    migration001.down();
-    break;
-  default:
+const migrations = [migration001, migration002];
+
+const runMigrations = async () => {
+  await client.connect();
+  const [_, __, action] = process.argv;
+
+  if (action !== "up" && action !== "down") {
     throw new Error("UNKOWN ACTION!");
-}
+  }
+
+  if (action === "down") {
+    migrations.reverse();
+  }
+
+  const promises = migrations.map(migrations => migrations[action]());
+
+  await Promise.all(promises);
+
+  await client.end();
+
+  return `Migrate ${action}! ${migrations.length} migrations...`;
+};
+
+
+runMigrations().then((res) => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+});
